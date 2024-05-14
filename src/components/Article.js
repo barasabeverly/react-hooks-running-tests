3fcff5b06a6ff1da62c9276091faece5d1c19370
priscillah barasa <beverlybarasa@gmail.com>
2024-05-15 01:01:11 +0300

    running tests on react

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
